e88157958f0b890643f45b8cdf0e9cda5231a31f
arnepie <152080744+arnepie@users.noreply.github.com>
2024-08-21 13:22:01 +0200

    Update network.py

--- network.py
+++ network.py
@@ -5,7 +5,7 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.0.219"  # Insert Wi-Fi ip address
+        self.server = ""  # Insert Wi-Fi ip address
         self.port = 5555  # Free port
         self.addr = (self.server, self.port)
         self.p = self.connect()
